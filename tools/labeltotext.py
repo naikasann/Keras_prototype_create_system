458b0f108eab47ae32729d1f20ab6b60117571ec
yamazaki <51977859+naikasann@users.noreply.github.com>
2020-05-10 00:30:58 +0900

    :zap: Segnetのためにラベル付けしないものの作成

--- tools/labeltotext.py
+++ tools/labeltotext.py
@@ -1,12 +1,14 @@
 import os
 import cv2
+from tqdm import tqdm
 
-path = "./val_dataset"
-textfile = "val_dataset.txt"
+path = ""
+textfile = ".txt"
 
 f = open(textfile, "w")
 
-filedir = os.listdir(path)
+### [画像パス] [ラベルつけ]を行う。
+"""filedir = os.listdir(path)
 for cnt, labels in enumerate(filedir):
     labels_path = os.path.join(path,labels)
     images = os.listdir(labels_path)
@@ -14,4 +16,15 @@ for cnt, labels in enumerate(filedir):
         imagepath = os.path.join(labels_path, imagepath)
 
         string = imagepath + " " + str(cnt) + "\n"
-        f.writelines(string)
+        f.writelines(string)"""
+
+# 指定したフォルダーの内の画像をすべてテキストに書き込む
+"""
+filedir = os.listdir(path)
+print("File count :", len(filedir))
+for imagepath in tqdm(filedir):
+    imagepath = os.path.join(path, imagepath)
+
+    string = imagepath + "\n"
+    f.writelines(string)
+"""
--- tools/resizeprog.py
+++ tools/resizeprog.py
@@ -1,9 +1,12 @@
 import os
 import cv2
+from tqdm import tqdm
 
-path = "./dataset"
-size = (227, 227)
+path = ""
+size = (000, 000)
 
+### [画像パス] [ラベルつけ]を行う。
+"""
 filedir = os.listdir(path)
 for labels in filedir:
     labels_path = os.path.join(path,labels)
@@ -14,3 +17,16 @@ for labels in filedir:
         img = cv2.imread(imagepath, cv2.IMREAD_COLOR)
         new_img = cv2.resize(img, size, interpolation = cv2.INTER_LINEAR)
         cv2.imwrite(imagepath, new_img)
+"""
+
+# 指定したフォルダーの内の画像をすべてテキストに書き込む
+"""
+filedir = os.listdir(path)
+print("File count :", len(filedir))
+for imagepath in tqdm(filedir):
+    imagepath = os.path.join(path, imagepath)
+
+    img = cv2.imread(imagepath, cv2.IMREAD_COLOR)
+    new_img = cv2.resize(img, size, interpolation = cv2.INTER_LINEAR)
+    cv2.imwrite(imagepath, new_img)
+"""
