6e936486c91f96667d9c74318ad2271705ac46ca
yamazaki <51977859+naikasann@users.noreply.github.com>
2020-07-31 02:00:30 +0900

    :ambulance: :boom: DatasetGeneatorの大幅な換装
    自作してきたジェネレータ型インスタンスを生成するメソッドを解体。
    flowfromdataframeを用いたデータ読み取りに換装(ジェネレータなのは変わらない)
    Imagedatagenを用いたデータ水増しを可能に
    BCLerearingのプログラムを別クラスに移動(他のメソッドに影響が発生するため)
    使用感は以前とあまり変わらないはず。

--- train/datasetgenerator/DatasetGenerator.py
+++ train/datasetgenerator/DatasetGenerator.py
@@ -1,25 +1,13 @@
 import tensorflow.keras as keras
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
-from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-import numpy as np
-import cv2
+import pandas as pd
 import os
-import random
 
 class DatasetGenerator:
-    # initial function
-    def __init__(self):
-        self.reset()
-
-    # Empty the images list & labels list.
-    def reset(self):
-        self.images = []
-        self.labels = []
-
     # for test program. It's better to alter it later.
     def text_dataset(self, resourcepath):
-        input, answer = [], []
+        train_data = []
         # open text file.
         with open(resourcepath) as f:
             readlines = f.readlines()
@@ -27,131 +15,30 @@ class DatasetGenerator:
         # Input data.
         for line in readlines:
             linebuffer = line.split(" ")
-            input.append(linebuffer[0])
-            answer.append(linebuffer[1])
-            # return [imagepath], label
-        return input, answer
-
-    # count the number of data.
-    def text_datacounter(self,datapath):
-        # open resorse.
-        with open(datapath) as f:
-            readlines = f.readlines()
-        # Returning the number of data
-        return len(readlines)
-
-    # Generator to read text.
-    def text_dataset_generator(self, resourcepath, input_shape, classes, batchsize, shuffle=True):
-        # open resorse.
-        with open(resourcepath) as f:
-            readlines = f.readlines()
-
-        # data shuffle.
-        if shuffle:
-            random.shuffle(readlines)
-
-        # loop for generator.
-        while True:
-            for line in readlines:
-                # ["image path" "label"] => linebuffer
-                linebuffer = line.split(" ")
-                try:
-                    # I'll load the image, and if it doesn't work, I'll terminate the program.
-                    image = img_to_array(load_img(linebuffer[0], target_size=input_shape))
-                except Exception as e:
-                    print("Failed to load data.")
-                    print("ERROR : ", e)
-                    exit(1)
-                 # Normalize image.
-                image /= 255.0
-                # list append.
-                self.images.append(image)
-                self.labels.append(to_categorical(linebuffer[1], len(classes)))
-
-                # When the batch size is reached, it yields.
-                if(len(self.images) == batchsize):
-                    # Convert to Numpy array type
-                    inputs = np.asarray(self.images, np.float32)
-                    targets = np.asarray(self.labels, np.float32)
-                    # Resetting an Instance
-                    self.reset()
-                    # for generator.
-                    yield inputs, targets
+            input = linebuffer[0]
+            answer = linebuffer[1].rstrip("\n")
+            train_data.append([input, str(answer)])
+            # return [imagepath], [label]
+        return train_data
 
     # for test program. It's better to alter it later.
     def onefolder_dataet(self, resourcepath, classes):
-        input , answer = [], []
+        train_data = []
 
         input = os.listdir(resourcepath)
         for image in input:
             for count, category,  in enumerate(classes):
                 # Find out which category. Identify the image name and assign a label.
                 if category in image:
-                    answer.append(count)
+                    input = image
+                    answer = count
+                    train_data.append([input, str(answer)])
         # return [imagepath], [label]
-        return input, answer
-
-    # count the number of data.
-    def onefolder_datacounter(self, datapath):
-        image_list = os.listdir(datapath)
-        return len(image_list)
-
-    # Generator to read folder.
-    def onefolder_dataset_generator(self, resourcepath, input_shape, classes, batchsize, shuffle=True):
-        # Refers to all the contents of a folder. (Assign a list of image paths.)
-        image_list = os.listdir(resourcepath)
-
-        # data shuffle.
-        if shuffle:
-            random.shuffle(image_list)
-
-        # for generator.
-        while True:
-            for image_path in image_list:
-                # image path read.
-                image_path = os.path.join(resourcepath, image_path)
-                try:
-                    # I'll load the image, and if it doesn't work, I'll terminate the program.
-                    image = img_to_array(load_img(image_path, target_size=input_shape))
-                except Exception as e:
-                    print("Failed to load data.")
-                    print("ERROR : ", e)
-                    exit(1)
-                # Normalize image.
-                image /= 255
-                # list append.
-                self.images.append(image)
-
-                append = False
-                # Labeling based on the name of the image and the name of the category.
-                for count, category in enumerate(classes):
-                    # Check each category to see which label it was.
-                    if category in image_path:
-                        # Assigning labels.
-                        self.labels.append(to_categorical(count, len(classes)))
-                        # Assign "True" to the variable to check if the category is added or not.
-                        append = True
-                        break
-                # Check the success of the label assignment.
-                if not append:
-                    print("Failed to assign a label.")
-                    exit(1)
-                else:
-                    # Resetting the check for non-applicable labels.
-                    append = False
-
-                # When the batch size is reached, it yields.
-                if(len(self.images) == batchsize):
-                    inputs = np.asarray(self.images, np.float32)
-                    targets = np.asarray(self.labels, np.float32)
-                    self.reset()
-
-                    yield inputs, targets
+        return train_data
 
     # for test program. It's better to alter it later.
     def folder_dataset(self, resourcepath):
-        input = []
-        answer = []
+        train_data = []
 
         # folder check.
         labeldir = os.listdir(resourcepath)
@@ -161,101 +48,92 @@ class DatasetGenerator:
             images = os.listdir(label_path)
             # Extracts data from a set of image files.
             for imagepath in images:
-                input.append(os.path.join(label_path, imagepath))
-                answer.append(category)
+                input = os.path.join(label_path, imagepath)
+                answer = category
+                train_data.append([input, str(answer)])
         # return [imagepath], [label]
-        return input, answer
+        return train_data
 
-    # count the number of data.
-    def folder_datacounter(self, datapath):
-        imagespath = []
+    def imagedatagenerator(self, yml, isValidation=False):
+        # use image data generator(use padding?)
+        if isValidation:
+            datagen = ImageDataGenerator(rescale = 1./255)
+        else:
+            if yml["Trainsetting"]["useAugment"]:
+                print("Data augmentation using a data generator")
+                params = {
+                    "rescale"                       : 1./255,
+                    "featurewise_center"            : yml["Trainsetting"]["featurewise_center"],
+                    "samplewise_center"             : yml["Trainsetting"]["samplewise_center"],
+                    "featurewise_std_normalization" : yml["Trainsetting"]["featurewise_std_normalization"],
+                    "samplewise_std_normalization"  : yml["Trainsetting"]["samplewise_std_normalization"],
+                    "zca_whitening"                 : yml["Trainsetting"]["zca_whitening"],
+                    "rotation_range"                : yml["Trainsetting"]["rotation_range"],
+                    "width_shift_range"             : yml["Trainsetting"]["width_shift_range"],
+                    "height_shift_range"            : yml["Trainsetting"]["height_shift_range"],
+                    "horizontal_flip"               : yml["Trainsetting"]["horizontal_flip"],
+                    "vertical_flip"                 : yml["Trainsetting"]["vertical_flip"],
+                    "validation_split"              : yml["Trainsetting"]["validation_split"]
+                }
+                datagen = ImageDataGenerator(**params)
+            else:
+                print("Operate as a generator without data augmentation")
+                datagen = ImageDataGenerator(rescale = 1./255)
 
-        # folder check.
-        labeldir = os.listdir(datapath)
-        # Loop the number of label folders.
-        for labels in labeldir:
-            label_path = os.path.join(datapath, labels)
-            images = os.listdir(label_path)
-            # Extracts data from a set of image files.
-            for imagepath in images:
-                imagespath.append(os.path.join(label_path, imagepath))
+        # Avoiding Lint
+        train_data = None
+        if isValidation:
+            # Create dataset generator
+            if yml["Validation"]["readdata"] == "text" or yml["Validation"]["readdata"] == "TEXT":
+                train_data = self.text_dataset(yml["Validation"]["resourcepath"])
+            elif yml["Validation"]["readdata"] == "onefolder" or yml["Validation"]["readdata"] == "Onefolder":
+                train_data = self.onefolder_dataet(yml["Validation"]["resourcepath"],
+                                                    yml["Resourcedata"]["classes"])
+            elif yml["Validation"]["readdata"] == "folder" or yml["Validation"]["readdata"] == "Folder":
+                train_data = self.folder_dataset(yml["Validation"]["resourcepath"])
+            else:
+                print("It seems to have input an unsupported generator name. Stops the program.")
+        else:
+            # Create dataset generator
+            if yml["Resourcedata"]["readdata"] == "text" or yml["Resourcedata"]["readdata"] == "TEXT":
+                train_data = self.text_dataset(yml["Resourcedata"]["resourcepath"])
+            elif yml["Resourcedata"]["readdata"] == "onefolder" or yml["Resourcedata"]["readdata"] == "Onefolder":
+                train_data = self.onefolder_dataet(yml["Resourcedata"]["resourcepath"],
+                                                    yml["Resourcedata"]["classes"])
+            elif yml["Resourcedata"]["readdata"] == "folder" or yml["Resourcedata"]["readdata"] == "Folder":
+                train_data = self.folder_dataset(yml["Resourcedata"]["resourcepath"])
+            else:
+                print("It seems to have input an unsupported generator name. Stops the program.")
+                exit(1)
 
-        return len(imagespath)
+        # take inputshape(width, height) and classes(list)
+        inputshape = (yml["Resourcedata"]["img_row"], yml["Resourcedata"]["img_col"])
 
-    # Generator to read folder.
-    def folder_dataset_generator(self, resourcepath, input_shape, classes, batchsize, shuffle=True):
-        imagespath = []
-        labels = []
+        # choice class mode(class 2 => binary, other => categorical)
+        classes = yml["Resourcedata"]["classes"]
+        print("=class name=")
+        for i, c in enumerate(classes):
+            print("{} => {}(Convert later to a one-hot expression)".format(c, i))
+        classes = [str(i) for i in range(len(classes))]
+        if len(classes) == 2:
+            classmode = "binary"
+        else:
+            classmode = "categorical"
+        print("class mode : {}".format(classmode))
 
-        # folder check.
-        labeldir = os.listdir(resourcepath)
-        # Loop the number of label folders.
-        for category, labels in enumerate(labeldir):
-            label_path = os.path.join(resourcepath, labels)
-            images = os.listdir(label_path)
-            # Extracts data from a set of image files.
-            # [imagepath], [label]
-            for imagepath in images:
-                imagespath.append(os.path.join(label_path, imagepath))
-                labels.append(category)
+        # check number of data
+        datacount = len(train_data)
+        print("number of data : {}".format(datacount))
+        print("============")
 
-        if shuffle:
-            trainlist = list(zip(imagespath, labels))
-            random.shuffle(trainlist)
-            imagespath, labels = zip(*trainlist)
-
-        # for genarator.
-        while True:
-            for image, label in zip(imagespath, labels):
-                # input image & label.
-                self.images.append(img_to_array(load_img(image, target_size=input_shape)) / 255.)
-                self.labels.append(to_categorical(label, len(classes)))
-
-                # When the batch size is reached, it yields.
-                if(len(self.images) == batchsize):
-                    inputs = np.asarray(self.images, np.float32)
-                    targets = np.asarray(self.labels, np.float32)
-                    self.reset()
-
-                    yield inputs, targets
-
-    def bclearning_generator(self, resourcepath, input_shape, classes, batchsize, shuffle=True):
-        # Retrieves a generator function in a class.
-        generator = self.text_dataset_generator(resourcepath, input_shape, classes, batchsize, shuffle)
-        while True:
-            # Take out the original image for mixing.
-            original_images , original_labels = next(generator)
-            # onehot label => label
-            labels = np.argmax(original_labels, axis = 1)
-            for original_image, label, original_label in zip(original_images, labels, original_labels):
-                # List whether it is a different category or not
-                diff_labels = np.where(labels != label)
-                # Randomly select the images to be mixed from the list.
-                mix_idx = np.random.choice(diff_labels[0].tolist())
-                # Generates a random percentage of images to be mixed.
-                random_proportion = np.random.rand()
-                # The amount of features of the main image is set to be more than that of the main image.
-                # (may not be necessary?)
-                if random_proportion < 0.5:
-                    random_proportion - 1
-                # Mix the images and labels.
-
-                mix_img = random_proportion * original_image + (1 - random_proportion) * original_images[mix_idx]
-                mix_label = random_proportion * original_label + (1 - random_proportion) * original_labels[mix_idx]
-
-                # Save the BCLearning image appropriately.
-                # Basically, comments, if you don't stop the program in the middle, the image will swell up.
-                # mix_img = cv2.cvtColor(mix_img, cv2.COLOR_BGR2RGB)
-                # cv2.imwrite("./bclearning/bcresult" + str(random_proportion) + ".jpg", mix_img * 255)
-
-                # input image & label.
-                self.images.append(mix_img)
-                self.labels.append(mix_label)
-
-                # When the batch size is reached, it yields.
-                if(len(self.images) == batchsize):
-                    inputs = np.asarray(self.images, np.float32)
-                    targets = np.asarray(self.labels, np.float32)
-                    self.reset()
-
-                    yield inputs, targets
+        dataframe = pd.DataFrame(train_data, index = None, columns = ["filename", "class"])
+        generator = datagen.flow_from_dataframe(dataframe,
+                                                x_col="filename",
+                                                y_col="class",
+                                                target_size=inputshape,
+                                                color_mode="rgb",
+                                                classes=classes,
+                                                class_mode=classmode,
+                                                batch_size=yml["Trainsetting"]["batchsize"],
+                                                shuffle=yml["Trainsetting"]["shuffle"])
+        return generator, datacount
